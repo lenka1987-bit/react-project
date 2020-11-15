b300e827f4a9d9e6ae90362adc6061a77dc42b3f
LenkaDoubravova <lenka.doubravova@gmail.com>
2020-11-15 22:47:09 +0100

    add background and final modification

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York"/>
-      <footer><small>Coded by Lenka Doubravova</small></footer> 
+        <Weather defaultCity="Piestany"/>
+      <footer><small>This is <a href="https://github.com/lenka1987-bit/react-project">open-source</a> coded by Lenka Doubravova</small></footer> 
      </div>   
     </div>
   );
